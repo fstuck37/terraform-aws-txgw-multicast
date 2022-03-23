bd3d89a79dc51903660eb0ef4afdc4ee94535c3e
Stuck, Fred <StuckF@DNB.com>
2022-03-23 18:20:36 -0400

    init

--- multicast_domain.tf
+++ multicast_domain.tf
@@ -13,9 +13,29 @@ resource "aws_ec2_transit_gateway_multicast_domain_association" "multicast_domai
     transit_gateway_multicast_domain_id = aws_ec2_transit_gateway_multicast_domain.multicast_domain.id
 }
 
-resource "aws_ec2_transit_gateway_multicast_group_member" "member1" {
+resource "aws_ec2_transit_gateway_multicast_group_member" "group_member" {
   for_each = var.multicast_group_members
     group_ip_address                    = each.value
     network_interface_id                = each.key
     transit_gateway_multicast_domain_id = aws_ec2_transit_gateway_multicast_domain_association.multicast_domain_association.transit_gateway_multicast_domain_id
+}
+
+
+data "aws_subnet" "subnets" {
+  for_each = { for sn in var.subnet_ids : sn => sn }
+   id = each.value
+}
+
+data "aws_network_interface" "enis" {
+  for_each = { for eni in keys(var.multicast_group_members) : eni => eni }
+  id = eni
+}
+
+locals {
+  eni-dmid = {
+    for_each eni in data.aws_network_interface.enis : eni.id => join(", " [
+      for_each sn in data.aws_subnet.subnets : sn
+        if sn.availability_zone == eni.availability_zone
+    ])
+  }
 }
