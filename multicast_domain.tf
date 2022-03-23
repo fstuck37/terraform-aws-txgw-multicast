694c33c4b2bba19d40db07d81c241f5293a892d5
Stuck, Fred <StuckF@DNB.com>
2022-03-23 18:22:39 -0400

    init

--- multicast_domain.tf
+++ multicast_domain.tf
@@ -33,8 +33,8 @@ data "aws_network_interface" "enis" {
 
 locals {
   eni-dmid = {
-    for_each eni in data.aws_network_interface.enis : eni.id => join(", ", [
-      for_each sn in data.aws_subnet.subnets : sn
+    for eni in data.aws_network_interface.enis : eni.id => join(", ", [
+      for sn in data.aws_subnet.subnets : sn
         if sn.availability_zone == eni.availability_zone
     ])
   }
